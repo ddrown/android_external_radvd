4dcb91d1c6c4b173d8a3a493e196e68fc968f349
Daniel Drown <dan-android@drown.org>
2012-04-27 11:05:43 -0500

    initial commit

---
+++ Android.mk
@@ -0,0 +1,14 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := log.c recv.c socket.c util.c device-common.c interface.c process.c radvd.c send.c timer.c device-linux.c netlink.c privsep-linux.c gram.c scanner.c
+
+LOCAL_MODULE := radvd
+
+LOCAL_CFLAGS := -O2 -g -W -Wall -DINET6=1 -DPATH_RADVD_CONF=\"$(PATH_RADVD_CONF)\" 
+LOCAL_SYSTEM_SHARED_LIBRARIES := libc
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_EXECUTABLE)
+
+
