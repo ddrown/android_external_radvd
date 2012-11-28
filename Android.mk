fa0fc1203325ac27aad488e90d202bff4d56000a
Daniel Drown <dan-android@drown.org>
2012-11-28 10:53:40 -0600

    build radvd on debug builds

--- Android.mk
+++ Android.mk
@@ -1,7 +1,9 @@
 LOCAL_PATH := $(call my-dir)
-
 include $(CLEAR_VARS)
+
 LOCAL_SRC_FILES := log.c recv.c socket.c util.c device-common.c interface.c process.c radvd.c send.c timer.c device-linux.c netlink.c privsep-linux.c gram.c scanner.c
+
+LOCAL_MODULE_TAGS := debug
 
 LOCAL_MODULE := radvd
 
